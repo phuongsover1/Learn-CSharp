a4d0688e062b96277dced94149ac73fdb1f278fd
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-10 14:31:40 +0700

    declared 2 int variables and print their sum.

---
+++ 02-Datatypes-And-Variables/02-Datatypes-And-Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace DatatypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // declaring a variable
+            int num1;
+            // assigning a value to a variable
+            num1 = 13;
+
+            // declaring and initializing a variable in one go
+            int num2 = 23;
+            int sum = num1 + num2; 
+
+            // using concatination
+            Console.WriteLine("num1: " + num1 + " + num2: " + num2 + " is " + sum);
+
+        }
+    }
+} 
