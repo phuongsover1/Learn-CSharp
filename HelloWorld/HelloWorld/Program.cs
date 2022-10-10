745faa7d28cf5abe0fa736cc6cab91ced6750c46
phuon <phuon@DESKTOP-NK2EH9I>
2022-10-10 14:08:30 +0700

    Add project files.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace HelloWorld
+{
+    public class Program
+    {
+        //static int age;
+        //static sbyte testSbyte = 129;
+        static double testSbyte = 12.3434;
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine(age);
+            Console.WriteLine(testSbyte);
+        }
+    }
+}
