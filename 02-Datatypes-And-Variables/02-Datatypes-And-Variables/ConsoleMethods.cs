2de80d377dd9f34b3ed515298f19ccf4acc48149
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-10 17:06:37 +0700

    Error when changing console's foreground and backgroundColor.

--- 02-Datatypes-And-Variables/02-Datatypes-And-Variables/ConsoleMethods.cs
+++ 02-Datatypes-And-Variables/02-Datatypes-And-Variables/ConsoleMethods.cs
@@ -10,17 +10,28 @@ namespace _02_Datatypes_And_Variables
     {
         public static void Main(string[] args)
         {
-            Console.Write("Enter a string and press enter:");
-            string readInput = Console.ReadLine();
-            Console.WriteLine("You have entered {0}", readInput);
+            /*            Console.Write("Enter a string and press enter:");
+                        string readInput = Console.ReadLine();
+                        Console.WriteLine("You have entered {0}", readInput);
 
-            Console.Write("Enter any key and press enter:");
-            int asciiValue = Console.Read();
-            Console.WriteLine("ASCII value is {0}:", asciiValue);
-
+                        Console.Write("Enter any key and press enter:");
+                        int asciiValue = Console.Read();
+                        Console.WriteLine("ASCII value is {0}:", asciiValue);
+            */
             //ConsoleKeyInfo consoleKey = Console.ReadKey();
             //Console.WriteLine("consoleKey.KeyChar: {0}", consoleKey.KeyChar); 
             //Console.WriteLine("consoleKey.Key: {0}", consoleKey.); 
+
+            ChangeConsoleColor();
+        }
+
+        public static void ChangeConsoleColor()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Hello Phuong");
+            Console.WriteLine("C# is cool!");
         }
     }
 }
