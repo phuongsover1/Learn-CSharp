4a73dd2ccce152d399097dd2a7343e903cbd5ac2
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-12 10:44:43 +0700

    Learned how to cast one type to another type explicitly

--- 02-Datatypes-And-Variables/02-Datatypes-And-Variables/ConsoleMethods.cs
+++ 02-Datatypes-And-Variables/02-Datatypes-And-Variables/ConsoleMethods.cs
@@ -8,27 +8,29 @@ namespace _02_Datatypes_And_Variables
 {
     internal class ConsoleMethods
     {
-        public static void Main(string[] args)
+/*        public static void Main(string[] args)
         {
-            /*            Console.Write("Enter a string and press enter:");
+            *//*            Console.Write("Enter a string and press enter:");
                         string readInput = Console.ReadLine();
                         Console.WriteLine("You have entered {0}", readInput);
 
                         Console.Write("Enter any key and press enter:");
                         int asciiValue = Console.Read();
                         Console.WriteLine("ASCII value is {0}:", asciiValue);
-            */
+            *//*
             //ConsoleKeyInfo consoleKey = Console.ReadKey();
             //Console.WriteLine("consoleKey.KeyChar: {0}", consoleKey.KeyChar); 
             //Console.WriteLine("consoleKey.Key: {0}", consoleKey.); 
 
             ChangeConsoleColor();
         }
-
+*/
         public static void ChangeConsoleColor()
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.BackgroundColor = ConsoleColor.Yellow;
+
+            Console.Clear();
 
             Console.WriteLine("Hello Phuong");
             Console.WriteLine("C# is cool!");
---
+++ 02-Datatypes-And-Variables/02-Datatypes-And-Variables/ExplicitAndImplicitConversion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Datatypes_And_Variables
+{
+    internal class ExplicitAndImplicitConversion
+    {
+        public static void Main(string[] args)
+        {
+            double myDouble = 13.37;
+            int myInt;
+
+            // cast double to int explicitly
+            myInt = (int)myDouble;
+            Console.WriteLine(myInt);
+        }
+    }
+}
