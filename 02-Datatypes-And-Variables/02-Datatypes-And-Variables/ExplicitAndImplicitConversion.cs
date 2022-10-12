2eede4c87ab7434e29f69ff20fa8cfb36111550f
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-12 10:58:27 +0700

    Learned converting some type to string type.

--- 02-Datatypes-And-Variables/02-Datatypes-And-Variables/ExplicitAndImplicitConversion.cs
+++ 02-Datatypes-And-Variables/02-Datatypes-And-Variables/ExplicitAndImplicitConversion.cs
@@ -21,7 +21,17 @@ namespace _02_Datatypes_And_Variables
             int num = 12345432;
             long bigNum = num;
 
-            double bigDouble = bigNum; 
+            double bigDouble = bigNum;
+            Console.WriteLine("int: {0}, long: {1}, double: {2}", num, bigNum, bigDouble);
+
+            // typeConversion
+            string myString = bigDouble.ToString();
+            string myString1 = myDouble.ToString();
+            Console.WriteLine("myString: {0}, myString1: {1}", myString, myString1);
+
+            bool sunIsShining = true;
+            string myBoolString = sunIsShining.ToString();
+            Console.WriteLine("myBoolString: " + myBoolString);
         }
     }
 }
