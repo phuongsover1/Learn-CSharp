9ca0c41243c9db48f91620ef25545eddfcb4f3ad
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-13 17:58:34 +0700

    Learned some string manipulation methods.

---
+++ 02-Datatypes-And-Variables/02-Datatypes-And-Variables/StringManipulation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Datatypes_And_Variables
+{
+    internal class StringManipulation
+    {
+        public static void Main(string[] args)
+        {
+            // define few variables
+            int age = 31;
+            string name = "Allfonso";
+
+            // 1. string concatenation
+            Console.WriteLine("String Concatenation");
+            Console.WriteLine("Hello my name is " + name + " , I am " + age + " years old");
+
+            // 2. String formatting.
+            // string formatting uses index
+            Console.WriteLine("String Formatting");
+            Console.WriteLine("Hello my name is {0}, I am {1} years old.", name, age);
+
+            // 3. String interpolation.
+            // string Interpolation uses $ at the start which will aloow us to write our 
+            // variables like this {variableName}
+            Console.WriteLine("String Interpolation");
+            Console.WriteLine($"Hello my name is {name}, I am {age} years old.");
+
+            // 4. Verbatim strings.
+            // verbatim strings start with @ and tells the compiler to take the string 
+            // literally and ignore any spaces and escape characters like \n
+            Console.WriteLine("Verbatim strings");
+            Console.WriteLine(@"Lorem ip sum dolir sit am\et,
+dfdkfjkd
+dfjdk \n fdkfjkdjf");
+    
+        }
+    }
+}
