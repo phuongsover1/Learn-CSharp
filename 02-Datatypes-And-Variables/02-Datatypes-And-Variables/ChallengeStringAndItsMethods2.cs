52228593f2a2870ba927a052eab781dc04419f60
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-13 22:44:43 +0700

    Teacher's solution.

--- 02-Datatypes-And-Variables/02-Datatypes-And-Variables/ChallengeStringAndItsMethods2.cs
+++ 02-Datatypes-And-Variables/02-Datatypes-And-Variables/ChallengeStringAndItsMethods2.cs
@@ -12,16 +12,26 @@ namespace _02_Datatypes_And_Variables
         {
             Console.Write("Enter a string here:");
             string aString = Console.ReadLine();
-            Console.Write("Enter a string you want to search in the previous typed string:");
+            //Console.Write("Enter a string you want to search in the previous typed string:");
 
-            string wantToSearchString = Console.ReadLine();
-            Console.WriteLine("The index of the first occurrence of the searched character from string:{0}", aString.IndexOf(wantToSearchString));
+            //string wantToSearchString = Console.ReadLine();
+            //Console.WriteLine("The index of the first occurrence of the searched character from string:{0}", aString.IndexOf(wantToSearchString));
+            Console.Write("Enter a Character to search:");
+            // Read the character input to search
+            char searchInput = Console.ReadLine()[0];
+
+            // Gets the Index of the character from the string.
+            sbyte searchIndex = (sbyte)aString.IndexOf(searchInput);
+
+            // Prints the Index as a search result on console.
+            Console.WriteLine("Index of character {0} in string is {1}",searchInput,searchIndex);   
 
             Console.Write("Enter first name:");
             string firstName = Console.ReadLine();
             Console.Write("Enter last name:");
             string lastName = Console.ReadLine();
             string fullName = String.Concat(firstName, " ", lastName);
+            Console.WriteLine("Your full name is:{0}", fullName);
         }
     }
 }
