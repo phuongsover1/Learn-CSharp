46b852344ae8b6195b2b643596d6e96fbda6d207
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-17 10:37:30 +0700

    Learned how to return value from method

--- 03-function-and-methods/03-function-and-methods/Program.cs
+++ 03-function-and-methods/03-function-and-methods/Program.cs
@@ -6,9 +6,29 @@ namespace _03function_and_methods_
     {
         public static void Main(string[] args)
         {
-            WriteSomeThing();
-            WriteSomethingSpecific("I am an argument and am called from a method");
+            //writesomething();
+            //writesomethingspecific("i am an argument and am called from a method");
+            int result = Add(15, 31);
+            Console.WriteLine(result);
 
+            Console.WriteLine(Add(Add(1, 2), Add(3, 4)));
+
+
+            Console.WriteLine("Devide: {0}", Devide(3, 4));
+
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Devide(int num1, int num2)
+        {
+            return num1 / (double)num2;
         }
 
         // access modifier (static) return type method name (parameter1, parameter2)
