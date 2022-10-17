4b57319daed7e0508b3f3a4c853ff3fb41c5ac3b
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-17 12:48:47 +0700

    Learned how to create class and object from class

---
+++ 06-object-oriented/06-object-oriented/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_object_oriented
+{
+    // this calss is a blueprint for a datatype
+    internal class Human
+    {
+        // member variable
+        public string  firstName;
+
+        // member 
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi my name is {0}", firstName);
+
+        }
+
+    }
+}
---
+++ 06-object-oriented/06-object-oriented/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using _06_object_oriented;
+
+namespace _06_object_oriented_
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // create an object of my class
+            // an instance of Human
+            Human denis = new Human();
+            denis.firstName = "Phuong";
+            denis.IntroduceMyself();
+
+
+
+        }
+        
+
+    }
+}
