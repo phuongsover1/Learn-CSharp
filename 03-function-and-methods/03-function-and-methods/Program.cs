d91d9aa1f2e838eec2fac70f393e23da59ed98da
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-17 10:24:42 +0700

    Learned how to create void methods and called it

---
+++ 03-function-and-methods/03-function-and-methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03function_and_methods_
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteSomeThing();
+            WriteSomethingSpecific("I am an argument and am called from a method");
+
+        }
+
+        // access modifier (static) return type method name (parameter1, parameter2)
+        public static void WriteSomeThing()
+        {
+            Console.WriteLine("I am called from a method");
+            
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+    }
+}
