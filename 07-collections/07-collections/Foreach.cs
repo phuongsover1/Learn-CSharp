79c680c07a75c721ce0563e0061f3a6fdc56fb94
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-17 17:23:03 +0700

    foreach loop and switch statement challenge.

--- 07-collections/07-collections/Foreach.cs
+++ 07-collections/07-collections/Foreach.cs
@@ -8,30 +8,30 @@ namespace _07_collections
 {
     internal class Foreach
     {
-        public static void Main(string[] args)
-        {
-            //int[] nums = new int[10];
+        //public static void Main(string[] args)
+        //{
+        //    //int[] nums = new int[10];
 
 
-            //for (int i = 1; i <= nums.Length; ++i)
-            //{
-            //    nums[i - 1] = i;
-            //}
+        //    //for (int i = 1; i <= nums.Length; ++i)
+        //    //{
+        //    //    nums[i - 1] = i;
+        //    //}
 
-            //foreach (int k in nums)
-            //{
-            //    Console.WriteLine(k);
-            //}
+        //    //foreach (int k in nums)
+        //    //{
+        //    //    Console.WriteLine(k);
+        //    //}
 
-            // Create an array with 5 of your best friends
-            // Create a foreach loop which greets all of them
+        //    // Create an array with 5 of your best friends
+        //    // Create a foreach loop which greets all of them
 
-            string[] myfriends = { "Khang", "Long" , "Luu", "Quy", "Thinh"};
-            foreach(string name in myfriends)
-            {
-                Greeting(name);
-            }
-        }
+        //    string[] myfriends = { "Khang", "Long" , "Luu", "Quy", "Thinh"};
+        //    foreach(string name in myfriends)
+        //    {
+        //        Greeting(name);
+        //    }
+        //}
 
         private static void Greeting(string name)
         {
---
+++ 07-collections/07-collections/ForeachLoopAndSwitchStatement.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_collections
+{
+    internal class ForeachLoopAndSwitchStatement
+    {
+        public static void Run()
+        {
+            bool valid = false;
+            string inputValueType;
+
+            Console.Write("Enter a value :");
+
+            string inputValue = Console.ReadLine();
+
+            Console.WriteLine("Select the Data type to validate the input you have entered");
+            Console.WriteLine("Press 1 for String");
+            Console.WriteLine("Press 2 for Integer");
+            Console.WriteLine("Press 3 for Boolean");
+
+            Console.Write("Enter :");
+            int inputType = Convert.ToInt32(Console.ReadLine());
+
+            switch(inputType)
+            {
+                case 1:
+                    // check for string
+                    valid = IsAllAlphabetic(inputValue);
+                    inputValueType = "String";
+                    break;
+                case 2:
+                    // check for integer
+                    int retValue = 0;
+                    valid = int.TryParse(inputValue, out retValue);
+                    inputValueType = "Integer";
+                    break;
+                case 3:
+                    // check for boolean
+                    bool retFlag = false;
+                    valid = Boolean.TryParse(inputValue, out retFlag);
+                    inputValueType = "Boolean";
+                    break;
+                default:
+                    inputValueType = "unknow";
+                    Console.WriteLine("Not able to detect the input type, something is wrong");
+                    break;
+            }
+
+            Console.WriteLine("You have entered a value : {0} ", inputValue);
+            if (valid)
+                Console.WriteLine("It is valid : {0}", inputValueType);
+            else
+                Console.WriteLine("It is an invalid  : {0}", inputValueType);
+        }
+
+        /// <summary>
+        /// function to check if the input string is a pure letter string. 
+        /// </summary>
+        /// <param name="value">value to check</param>
+        /// <returns>boolean: 
+        ///     - true: all the character in string is letter.
+        /// </returns>
+        static bool IsAllAlphabetic(string value)
+        {
+            foreach(char c in value)
+            {
+                if(!char.IsLetter(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
