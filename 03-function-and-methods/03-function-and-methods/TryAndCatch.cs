7541b7eb762a12bc0ef51eb5203f2a5e951382d2
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-17 11:59:49 +0700

    Create divider app with try catch.

--- 03-function-and-methods/03-function-and-methods/TryAndCatch.cs
+++ 03-function-and-methods/03-function-and-methods/TryAndCatch.cs
@@ -10,26 +10,66 @@ namespace _03_function_and_methods
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a number!");
+            while (true)
+            {
+                bool exit = true;
+                try
+                {
+                    Console.Write("Please enter the first number:");
+                    string num1Input = Console.ReadLine();
+                    int number1 = int.Parse(num1Input);
+
+                    Console.Write("Please enter the second number:");
+                    string num2Input = Console.ReadLine();
+                    int number2 = int.Parse(num2Input);
+
+                    Console.WriteLine("Result of num1/num2: {0}",Devide(number1, number2));
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a valid number!");
+                    ClearConsole();
+                    exit = false;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("The value is too large or too small for int32.");
+                    Console.WriteLine("Max value is {0}", int.MaxValue);
+                    Console.WriteLine("Min value is {0}", int.MinValue);
+                    ClearConsole();
+                    exit = false;
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine(e.Message);
+                    ClearConsole();
+                    exit = false;
+                }
+
+                if (exit)
+                    break;
+            }
+
+
+        }
+
+        private static void ClearConsole()
+        {
+            Console.ReadKey();
+            
+            Console.Clear();
+        }
+        private static double Devide(int num1, int num2)
+        {
             try
             {
-                string userInput = Console.ReadLine();
-                int userInputAsInt = int.Parse(userInput);
+                return num1 / (double)num2;
             }
-            catch (FormatException)
+            catch (DivideByZeroException)
             {
-                Console.WriteLine("Please enter a valid number next time.");
+                throw new DivideByZeroException("Couldn't divide because number 2 is zero");
             }
-            catch (OverflowException)
-            {
-                Console.WriteLine("The value is too large or too small for an int32. Maximum is {0} and minimum is {1}", int.MaxValue, int.MinValue);
-
-            }
-            finally
-            {
-                Console.WriteLine("This is called anyways!");
-            }
-
         }
     }
 }
