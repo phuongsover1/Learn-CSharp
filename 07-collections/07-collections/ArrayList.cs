3c3aa41bb51094c3cb60cd358712b5afb3d7dc80
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-17 21:39:47 +0700

    Learned how to use ArrayList.

---
+++ 07-collections/07-collections/ArrayList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_collections
+{
+    internal class _ArrayList
+    {
+        public static void Run()
+        {
+            // declaring an ArrayList with undefined amount ò object
+            ArrayList myArrayList = new ArrayList();
+            // declaring an ArrayList with defined amount of object;
+            ArrayList myArrayList2 = new ArrayList(100);
+
+            myArrayList.Add(25);
+            myArrayList.Add("Hello");
+            myArrayList.Add(13.37f);
+            myArrayList.Add(10.3);
+            myArrayList.Add(true);
+
+            double sum = 0;
+            foreach(object obj in myArrayList)
+            {
+                if (obj is int)
+                {
+                    sum += Convert.ToDouble(obj);
+                }else if (obj is double)
+                {
+                    sum += (double)obj;
+                }else if (obj is string)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
--- 07-collections/07-collections/Program.cs
+++ 07-collections/07-collections/Program.cs
@@ -16,7 +16,9 @@ namespace _07_collections
             //Params.ParamsMethod(1, 2, 3, 4, 5, 6);
             //Params.ParamsMethod();
 
-            Params.ParamsMethod2(1, 2, 'c', "Phuong", false, true, 1.234f, 234.34);
+            //Params.ParamsMethod2(1, 2, 'c', "Phuong", false, true, 1.234f, 234.34);
+
+            _ArrayList.Run();
         }
     }
 }
