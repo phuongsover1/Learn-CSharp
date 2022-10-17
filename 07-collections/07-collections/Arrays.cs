82a17e7a6908c7ace9b08bad77084c8b9db806fb
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-17 16:28:20 +0700

    Created an array and read it's length

---
+++ 07-collections/07-collections/Arrays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07_collections_
+{
+    public class Arrays
+    {
+        public static void Main(string[] args)
+        {
+            int[] grades = new int[5];
+
+            grades[0] = 20;
+            grades[1] = 15;
+            grades[2] = 12;
+            grades[3] = 9;
+            grades[4] = 7;
+
+
+            // another way of initializing an array
+            int[] gradesOfMathStudentsA = { 20, 13, 12, 8, 8 };
+
+            // third way of initializing an array
+            int[] gradesOfMathStudentsB = new int[] { 15, 20, 3, 17, 18, 15 };
+
+            // Getting the Length of an array
+            Console.WriteLine(gradesOfMathStudentsA.Length);
+        }
+    }
+}
