7a0f1136ff6881a397362557e9149d9189f096f0
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-17 12:23:55 +0700

    Using TryParse.

---
+++ 04-making-decision/04-making-decision/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04_making_decision_
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string numberAsString = "128";
+            int parsedValue;
+            bool success = int.TryParse(numberAsString, out parsedValue);
+
+            if (success)
+            {
+                Console.WriteLine("Parsing successful - number is " + parsedValue);
+            } 
+            else
+            {
+                Console.WriteLine("Parsing failed");
+            }
+        }
+    }
+}
