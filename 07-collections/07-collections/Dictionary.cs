d4de3db0afb6a9a9a9abf11c28b93db7b7a2146b
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-18 09:58:53 +0700

    some dictionary practice.

--- 07-collections/07-collections/Dictionary.cs
+++ 07-collections/07-collections/Dictionary.cs
@@ -10,13 +10,55 @@ namespace _07_collections
     {
         public static void Run()
         {
-            Dictionary<int, string> myDictionary = new Dictionary<int, string>();
-            myDictionary.Add(1, "one");
-            myDictionary.Add(2, "two");
+            Employee[] employees =
+            {
+                new Employee("CEO", "Gwym", 95),
+                new Employee("Manager", "Joe", 35),
+                new Employee("HR", "Lora", 32),
+                new Employee("Secretary", "Petra", 28),
+                new Employee("Lead Developer", "Artorias", 55),
+                new Employee("Intern", "Ernest", 22)
+            };
 
-            // retrieve value through key
-            Console.WriteLine(myDictionary[1]);
+            Dictionary<string, Employee> myDictionary = new Dictionary<string, Employee>();
 
+            foreach(Employee e in employees)
+            {
+                myDictionary.Add(e.Role, e);
+            }
+
+            Employee ceo = myDictionary["CEO"];
+            Console.WriteLine("Employee Name: {0}, Role: {1}, Salary: {2}", ceo.Name, ceo.Role, ceo.Salary);
+
+
+
+
+        }
+    }
+    class Employee
+    {
+        public string Role { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public float Rate { get; set; }
+        
+        public float Salary
+        {
+            get
+            {
+                return Rate * 8 * 5 * 4 * 12;
+            }
+        }
+
+        // simple constructor
+        public Employee(string role, string name, int age)
+        {
+            Role = role;
+            Name = name;
+            Age = age;
         }
     }
 }
