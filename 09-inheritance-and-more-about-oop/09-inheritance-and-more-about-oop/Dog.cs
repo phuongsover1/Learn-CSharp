bf9551b2fd77d602bf2ca5a02376be35a1b66603
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-18 12:44:36 +0700

    How to declare virtual methods for sub classes to override it.

---
+++ 09-inheritance-and-more-about-oop/09-inheritance-and-more-about-oop/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_inheritance_and_more_about_oop
+{
+    class Dog : Animal
+    {
+        public Dog(string name, int age) : base(name, age)
+        {
+
+        }
+
+    }
+}
---
+++ 09-inheritance-and-more-about-oop/09-inheritance-and-more-about-oop/VirtualOverride.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_inheritance_and_more_about_oop
+{
+    // name, age, isHungry
+    // makeSound,eat,play
+    class Animal
+    {
+        public string Name { get; set; }
+        public int age { get; set; }
+        public bool IsHungry { get; set; }
+
+        public Animal(string name, int age)
+        {
+            this.Name = name;
+            this.age = age;
+
+            // in our case all our animals are hungry by default
+            IsHungry = true;
+        }
+
+        // an empty virtual method MakeSound for sub classes to override
+        public virtual void MakeSound()
+        {
+            
+        }
+
+
+        // a virtual method called Eat which sub classes can override
+        public virtual void Eat()
+        {
+            //  check ì aninmal is hungry
+            if (IsHungry)
+            {
+                // if yes then print the name of the anima + "is eating"
+                Console.WriteLine($"{Name} is eating");
+            } else
+            {
+                // otherwise print that the animal is not hungry
+                Console.WriteLine($"{Name} is not hungry");
+            }
+
+        }
+
+        // virtual method Play
+        public virtual void Play()
+        {
+            Console.WriteLine($"{Name} is playing");
+        }
+    }
+}
