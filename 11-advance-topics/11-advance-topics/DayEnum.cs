e225de26007a1d78b458a9329f88bdeb516372ec
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-26 19:13:20 +0700

    Enum in C#

---
+++ 11-advance-topics/11-advance-topics/DayEnum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_advance_topics
+{
+    enum Day { Mon,Tue,Wed,Thu,Fri,Sat,Sun}
+}
--- 11-advance-topics/11-advance-topics/Program.cs
+++ 11-advance-topics/11-advance-topics/Program.cs
@@ -1,26 +1,21 @@
 ﻿using System;
-namespace _10_advance_topic
+namespace _11_advance_topics
 {
     class Program
     {
-        struct Game
-        {
-            public string name;
-            public string developer;
-            public double rating;
-            public string releaseDate;
-
-        }
 
         public static void Main(string[] args)
         {
-            Game game1;
-            game1.name = "Pokemon Go";
-            game1.rating = 3.5;
-            game1.developer = "Niantic";
-            game1.releaseDate = "01.07.2016";
+            Day fr = Day.Fri;
+            Day su = Day.Sun;
 
-            Console.WriteLine("Game 1's name is : {0}", game1.name);
+            Day a = Day.Fri;
+            if (a == fr)
+            {
+                Console.WriteLine("Equal!");
+            }
+            // print the int value
+            Console.WriteLine((int)Day.Fri);
         }
     }
 }
