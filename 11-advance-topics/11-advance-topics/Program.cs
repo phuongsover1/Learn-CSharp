b3aea6f52138e5a23f4e2dbaacd75358fa7c3df7
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-26 18:45:02 +0700

    Struct in C#.

---
+++ 11-advance-topics/11-advance-topics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace _10_advance_topic
+{
+    class Program
+    {
+        struct Game
+        {
+            public string name;
+            public string developer;
+            public double rating;
+            public string releaseDate;
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Game game1;
+            game1.name = "Pokemon Go";
+            game1.rating = 3.5;
+            game1.developer = "Niantic";
+            game1.releaseDate = "01.07.2016";
+
+            Console.WriteLine("Game 1's name is : {0}", game1.name);
+        }
+    }
+}
