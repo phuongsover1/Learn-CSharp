0dfcb5f04cd8ebc69e293a721e366adc41f48ba6
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-29 00:03:19 +0700

    My own delegate variable, callback to assign to delegate variable

---
+++ 12-events-and-delegates/12-events-and-delegates/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_events_and_delegates
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
--- 12-events-and-delegates/12-events-and-delegates/Program.cs
+++ 12-events-and-delegates/12-events-and-delegates/Program.cs
@@ -3,28 +3,62 @@ namespace _12_events_and_delegates
 {
     class Program
     {
+        // defining a delegate type called FilterDelegate that takes a person object and return a boolean
+        public delegate bool FilterDelegate(Person p);
         public static void Main(string[] args)
         {
-            // list of name 
-            List<string> names = new List<string>()
-            { "Aiden", "Sif",
-                "Walter", "Anatoli" };
+            // create 4 Person objects
+            Person p1 = new Person("Aiden", 41);
+            Person p2 = new Person("Sif", 69);
+            Person p3 = new Person("Walter", 12);
+            Person p4 = new Person("Anatoli", 25);
 
-            names.RemoveAll(Filter);
-            foreach(string s in names)
-            {
-                Console.WriteLine(s);
-            }
+            // add the objects to a lsit called people
+            List<Person> people = new List<Person>() { p1, p2, p3, p4 };
+
+            DisplayPeople("Kids", people, IsMinor);
+            DisplayPeople("Adults", people, IsAdult);
+
+                
 
 
         }
 
-        // a method called filter that takes a string
-        static bool Filter(string s)
+        // a method to display the list of people that passes the filter condition
+        // (returns true)
+        // this method will take a title to be displayed, the list of people,
+        // and a filter delegate
+        static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
         {
-            // return whether the string s contains the letter 'i'
-            // the Contains method will return a bool which we will return as well
-            return s.Contains('i');
+            // print the title
+            Console.WriteLine(title);
+
+            foreach(Person p in people)
+            {
+                // check if this person passes the filter
+                if (filter(p))
+                {
+                    // print the person's name and age
+                    Console.WriteLine("{0}, {1} years old", p.Name, p.Age);
+                }
+            }
         }
+
+        // =============== FILTERS ================
+        static bool IsMinor(Person p)
+        {
+            return p.Age < 18;
+        }
+
+        static bool IsAdult(Person p)
+        {
+            return p.Age >= 18;
+        }
+
+        static bool IsSenior(Person p)
+        {
+            return p.Age > 65;
+        }
+
     }
 }
