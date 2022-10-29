15766a405148cce738d31a48331df93779d8db26
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-29 16:21:55 +0700

    created using C# code.

---
+++ 13-learn-wpf-02/13-learn-wpf-02/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _13_learn_wpf_02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StackPanel stackPanel = new StackPanel();
+            this.AddChild(stackPanel);
+
+            TextBlock txt = new TextBlock();
+            txt.Text = "Hello World!";
+            txt.HorizontalAlignment = HorizontalAlignment.Center;
+
+            txt.Margin = new Thickness(30);
+            txt.Background = Brushes.AliceBlue;
+
+            stackPanel.Children.Add(txt);
+
+            ListBox list = new ListBox();
+            list.Height = 100;
+            list.Width = 100;
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                ListBoxItem listItem = new ListBoxItem();
+                listItem.Content = $"Item {i + 1}";
+                list.Items.Add(listItem);
+            }
+
+            stackPanel.Children.Add(list);
+        }
+    }
+}
