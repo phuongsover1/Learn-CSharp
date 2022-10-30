c9accd11daf3a75c87ef71ce964f320537d34c0e
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-30 12:45:02 +0700

    Used ListBox and attach Datasource to ListBox.ItemsSource

---
+++ 13-learn-wpf-08/13-learn-wpf-08/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _13_learn_wpf_08
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Match> matches = new List<Match>();
+            matches.Add(new Match() { Team1 = "Bayern Munich", Team2 = "Real Madrid", Score1 = 3, Score2 = 2, Completion = 85 });
+            matches.Add(new Match() { Team1 = "MU", Team2 = "MC", Score1 = 3, Score2 = 6, Completion = 85 });
+            lbMatches.ItemsSource = matches;
+        }
+    }
+}
