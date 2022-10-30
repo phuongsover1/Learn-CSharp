17042f4cf9dcea1f31182606c182cc97ba74ece3
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-30 12:59:49 +0700

    Add ShowSelectedI Item button.

--- 13-learn-wpf-08/13-learn-wpf-08/MainWindow.xaml.cs
+++ 13-learn-wpf-08/13-learn-wpf-08/MainWindow.xaml.cs
@@ -25,8 +25,20 @@ namespace _13_learn_wpf_08
             InitializeComponent();
             List<Match> matches = new List<Match>();
             matches.Add(new Match() { Team1 = "Bayern Munich", Team2 = "Real Madrid", Score1 = 3, Score2 = 2, Completion = 85 });
-            matches.Add(new Match() { Team1 = "MU", Team2 = "MC", Score1 = 3, Score2 = 6, Completion = 85 });
+            matches.Add(new Match() { Team1 = "PSG", Team2 = "Barca", Score1 = 3, Score2 = 6, Completion = 40 });
+            matches.Add(new Match() { Team1 = "BVB Dotmund", Team2 = "PSG", Score1 = 2, Score2 = 1, Completion = 35 });
+            matches.Add(new Match() { Team1 = "Machester United", Team2 = "Manchester City", Score1 = 3, Score2 = 6, Completion = 90 });
+                
             lbMatches.ItemsSource = matches;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Match match = lbMatches.SelectedItem as Match;
+            if (match != null)
+            {
+                MessageBox.Show($"Selected Match: {match.Team1} {match.Score1}:{match.Score2} {match.Team2}");
+            }
         }
     }
 }
