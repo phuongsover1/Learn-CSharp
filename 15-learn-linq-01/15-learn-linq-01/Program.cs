41f6e777784f5dfb3395dc247b3671dbd515f547
Phương Nguyễn <phuongsover2@gmail.com>
2022-10-31 22:26:21 +0700

    Get an array of odd numbers using LINQ.

---
+++ 15-learn-linq-01/15-learn-linq-01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_learn_linq_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            OddNumbers(numbers);
+
+            Console.Read();
+        }
+
+        static void OddNumbers(int[] numbers)
+        {
+            Console.WriteLine("Odd Numbers: ");
+
+            IEnumerable<int> oddNumbers = from number in numbers 
+                                          where number % 2 != 0 
+                                          select number;
+
+            foreach(int i in oddNumbers) { Console.WriteLine(i); }  
+        }
+    }
+}
